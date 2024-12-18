838d6d03eedd736268fc99c13d5cacf9ae3dc7c4
Felix Asante <felixasante2005@gmail.com>
2024-12-18 19:25:52 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,3 +1,31 @@
+package org.example;
 public class Book {
-    // This is your working class
+    private String title;
+    private String author;
+    private int yearPublished;
+    
+    public Book(String title, String author, int yearPublished){
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+
+    // Getters and setters
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+
+    public void displayBookDetails(){
+        System.out.println("The title of the book is " + title);
+        System.out.println("The author of the book is " + author);
+        System.out.println("The year the book was published is " + yearPublished);
+    }
+    
+    
 }
